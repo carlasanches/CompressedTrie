5ef4a976c198c6261147f2cd337b6337d17cab30
carlasanches <carlasanches8@gmail.com>
2020-05-12 17:00:30 -0300

    Insercao de uma palavra do arquivo
    Agora o programa lÃª um arquivo que contem uma palavra ea adiciona na
    trie.

--- main.c
+++ main.c
@@ -15,7 +15,17 @@ int main(){
     t = clock();
 
     FILE *pointer_txt;
-    char text[20];
+
+    char word[5];
+
+    CompressedTrieTree tree;
+    Ocurrence ocurrence;
+
+    ocurrence.ocurrences = (int*) malloc(sizeof(int));
+    ocurrence.word = (char*) malloc(5 * sizeof(char));
+
+    ocurrence.ocurrences[0] = 1;
+    ocurrence.length = 4;
 
     pointer_txt = fopen("test.txt","r");
 
@@ -24,80 +34,26 @@ int main(){
         return 1;
     }
 
-    while(fgets(text,20,pointer_txt) != NULL){
-        printf("%s", text);
+    while(fgets(word,20,pointer_txt) != NULL){
+        printf("%s", word);
+
+        int i;
+
+        for(i = 0; i < 4; i++){
+            ocurrence.word[i] = word[i];
+        }
     }
 
     fclose(pointer_txt);
 
-    /*CompressedTrieTree tree;
-    Ocurrence ocurrence, ocurrence2, ocurrence3;
-
-    ocurrence.ocurrences = (int*) malloc(sizeof(int));
-    ocurrence.word = (char*) malloc(5 * sizeof(char));
-
-    ocurrence2.ocurrences = (int*) malloc(sizeof(int));
-    ocurrence2.word = (char*) malloc(5 * sizeof(char));
-
-    ocurrence3.ocurrences = (int*) malloc(sizeof(int));
-    ocurrence3.word = (char*) malloc(5 * sizeof(char));
-
-    char palavra[4];
-
-    int i;
-    for(i = 0; i < 4; i++){
-        palavra[i] = 'a';
-    }
-
-    ocurrence.ocurrences[0] = 1;
-    ocurrence.length = 4;
-
-    for(i = 0; i < 4; i++){
-        ocurrence.word[i] = palavra[i];
-    }
-
     Initialize(&tree); /* ponteiro do parâmetro recebe o endereço de memória da variável tree.
                           Assim, ele consegue alterar os valores de tree na função Initialize */
 
-    /*Insert(&tree, ocurrence);
-
-    for(i = 0; i < 4; i++){
-        palavra[i] = 'b';
-    }
-
-    ocurrence2.ocurrences[0] = 1;
-    ocurrence2.length = 4;
-
-    for(i = 0; i < 4; i++){
-        ocurrence2.word[i] = palavra[i];
-    }
-
-    Insert(&tree, ocurrence2);
-
-    for(i = 2; i < 4; i++){
-        palavra[i] = '\0';
-    }
-
-   // for(i = 2; i < 4; i++){
-   //     palavra[i] = 'a';
-   // }
-
-    ocurrence3.ocurrences[0] = 1;
-    ocurrence3.length = 4;
-
-    for(i = 0; i < 4; i++){
-        ocurrence3.word[i] = palavra[i];
-    }
-
-    Insert(&tree, ocurrence3);
+    Insert(&tree, ocurrence);
 
     FreeMemory(tree.root);
     free(ocurrence.ocurrences);
     free(ocurrence.word);
-    free(ocurrence2.ocurrences);
-    free(ocurrence2.word);
-    free(ocurrence3.ocurrences);
-    free(ocurrence3.word);*/
 
     t = clock() - t;
     printf("%lf ms", (double) t);
